a20bcea18ae295c9dd9ba26f78ecb6c87686a654
thiniv2 <nthien098@gmail.com>
2019-05-26 21:02:23 +0300

    ruokahuone improvements
    added more cakes and their animations

--- kawaiiGarden/kawaiiGarden/Assets/RuokaHuone.cs
+++ kawaiiGarden/kawaiiGarden/Assets/RuokaHuone.cs
@@ -7,6 +7,8 @@ public class RuokaHuone : MonoBehaviour
     public GameObject RuokaHuoneUI;
 
     public GameObject TealCakeobj;
+    public GameObject GreenCakeobj;
+    public GameObject BrownCakeobj;
 
     public HoitoButton hoitoScript;
 
@@ -37,7 +39,37 @@ public class RuokaHuone : MonoBehaviour
         hoitoScript.GiveHappiness();
     }
 
-   public void Close()
+    public void GreenCake()
+    {
+        Invoke("GreenCakeEat", 0.6f);
+
+        TealCakeobj.SetActive(false);
+    }
+
+    public void GreenCakeEat()
+    {
+        TealCakeobj.SetActive(true);
+        anim.SetBool("GreenCake", true);
+
+        hoitoScript.GiveHappiness();
+    }
+
+    public void BrownCake()
+    {
+        Invoke("BrownCakeEat", 0.6f);
+
+        TealCakeobj.SetActive(false);
+    }
+
+    public void BrownCakeEat()
+    {
+        TealCakeobj.SetActive(true);
+        anim.SetBool("BrownCake", true);
+
+        hoitoScript.GiveHappiness();
+    }
+
+    public void Close()
     {
         RuokaHuoneUI.SetActive(false);
     }
